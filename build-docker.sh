3d8b38a666c43d9b2bb0058953028b4424bc95c5
Carlo <carlo.panzi@gmail.com>
2021-11-05 09:13:26 +0100

    build: correct version from lerna

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=$(cat package.json | jq -r '.version')
+VERSION=$(cat lerna.json | jq -r '.version')
 NAME=$(cat package.json | jq -r '.name')
 TAG="kajyr/${NAME}:${VERSION}"
 docker build -f Dockerfile --tag $TAG .
