e7fef662816e8f41055cf7fe247e02969cefc164
Carlo <carlo.panzi@gmail.com>
2022-10-24 15:44:38 +0200

    chore: Updated auto merge script

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from 'react-dom';
 // After
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
